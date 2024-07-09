2ceb8f742460335816ba82ed6a3213df216225af
hellodamii <daremiarchs@gmail.com>
2024-07-09 14:03:18 +0100

    Start

--- BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -9,16 +9,58 @@ import SwiftUI
 
 struct BarcodeScannerView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.opacity(0.7)
+                .ignoresSafeArea(.all)
+            VStack {
+                Button {
+                    
+                } label: {
+                    Label ("Enter manually", systemImage: "flashlight.off.fill")
+                }
+                .buttonBorderShape(.circle)
+                .foregroundColor(.white)
+                .buttonStyle(.bordered)
+                .controlSize(.extraLarge)
+                .tint(.newGray)
+                Spacer()
+                
+                
+                VStack {
+                    Text("Scan the QR code")
+                        .fontWeight(.medium)
+                        .font(.system(size: 18))
+                    Rectangle()
+                        .strokeBorder(style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round, dash: []))
+                        .foregroundColor(.newGray)
+                        .frame(width: 244, height: 244)
+                        .cornerRadius(36)
+                        .padding()
+                    
+                    Button {
+                        
+                    } label: {
+                        Label ("Enter manually", systemImage: "keyboard")
+                    }
+                    .foregroundColor(.white)
+                    .buttonBorderShape(.capsule)
+                    .buttonStyle(.bordered)
+                    .controlSize(.extraLarge)
+                    .tint(.newGray)
+                }
+                .padding(24)
+                
+                Spacer()
+                
+            }
+            
+            
         }
-        .padding()
+       
     }
 }
 
 #Preview {
     BarcodeScannerView()
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
 }
